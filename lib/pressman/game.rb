fcda470a15647441858d5f85df91df58b7ead6ea
Gordon Thiesfeld <gthiesfeld@gmail.com>
2010-11-08 17:42:14 -0600

    cleaned up Game#play, moved code out to #generate and #move

--- lib/pressman/game.rb
+++ lib/pressman/game.rb
@@ -16,34 +16,38 @@
       until @winner
         current, opponent = players
 
-        move = current.get_move # player gives move coordinates or resigns
-        
-        if current.resigned?
-          @winner = opponent
-          break 
-        end
+        # player gives move coordinates or resigns
+        coordinatess = current.get_move 
+        @winner = opponent and break if current.resigned?
 
-        Move.new(move.merge(:player => current, :board => @board)).execute
+        move(coordinatess)
+        generate(current) if 
 
-        if @board.empty_square_in_home_row?(current) && 
-          active_stone = @board.active_stone_in_opponents_home_row(current) &&
-          current.generate?
-
-          active_stone.deactivate
-          coords = @board.empty_squares_in(current.home_row)
-          generate_stone(:player => current, :dst => coords)
-        end
-        
         @winner = current unless @board.pieces?(opponent)
         players.reverse!
       end
       announce_winner
     end
 
+    def generate(player)
+      active_stone = @board.active_stone_in_opponents_home_row(current)
+      if @board.empty_square_in_home_row?(current) && 
+        active_stone && current.generate?
+
+        active_stone.deactivate
+        coords = @board.empty_squares_in(current.home_row)
+        generate_stone(:player => current, :dst => coords)
+      end
+    end      
+
     def generate_stone(opts)
       x,y = opts[:dst]
       color = opts[:player].color
       @board[x][y] = Stone.new(color)  
+    end
+
+    def move(opts)
+      Move.new(move.merge(:player => current, :board => @board)).execute
     end
 
     def announce_winner
